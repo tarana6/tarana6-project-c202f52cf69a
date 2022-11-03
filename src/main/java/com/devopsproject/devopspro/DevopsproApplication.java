1768453c6a3ba86830947ed1d06c41ed3479ec47
tarana6 <you@example.com>
2022-11-03 15:58:42 +0530

    first commit

---
+++ src/main/java/com/devopsproject/devopspro/DevopsproApplication.java
@@ -0,0 +1,13 @@
+package com.devopsproject.devopspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devopsproject/devopspro/DevopsproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsproject.devopspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
